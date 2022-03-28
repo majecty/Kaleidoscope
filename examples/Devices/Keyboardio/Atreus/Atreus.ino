7ae751ba5ecf3d7c721fae7d5d20c6f3197570df
Park Juhyung <majecty@gmail.com>
2022-03-28 09:23:56 +0900

    add shift and insert, pause

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -86,7 +86,7 @@ KEYMAPS(
                      ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
                      ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
        ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_Lang1,    Key_Enter ,Key_Esc    ,Key_Minus ,Key_Quote  ,Key_F14
+       ,Key_Lang1    ,Key_Enter ,Key_Esc    ,Key_Minus ,Key_Quote  ,Key_F14
   ),
 
   [SYMBOL] = KEYMAP_STACKED
@@ -110,17 +110,17 @@ KEYMAPS(
       ,MoveToLayer(QWERTY) ,M(MACRO_VERSION_INFO) ,___     ,___          ,___     ,___
 
                 ,Key_6         ,Key_7               ,Key_8           ,Key_9          ,Key_0
-                ,Key_LeftArrow ,Key_DownArrow       ,Key_UpArrow     ,Key_RightArrow ,Key_F10
+                ,Key_LeftArrow ,Key_DownArrow       ,Key_UpArrow     ,Key_RightArrow ,Key_LeftShift
       ,___      ,Key_F11       ,Key_F12             ,Key_F13         ,Key_F14        ,Key_Quote
       ,___      ,___           ,___                 ,___             ,___            ,___
    ),
 
    [PAREN] = KEYMAP_STACKED
    (
-       Key_Home        ,Key_End          ,___   ,___    ,___
-      ,Key_LessThan    ,Key_GreaterThan  ,___   ,___    ,___
-      ,Key_PageUp      ,Key_PageDown     ,___   ,___    ,___       ,___
-      ,___             ,___              ,___   ,___    ,___       ,___
+       Key_Home        ,Key_End          ,Key_PrintScreen,___    ,___
+      ,Key_LessThan    ,Key_GreaterThan  ,Key_Insert ,___    ,___
+      ,Key_PageUp      ,Key_PageDown     ,Key_Pause  ,___    ,___       ,___
+      ,___             ,___              ,___        ,___    ,___       ,___
 
              ,___   ,Key_LeftCurl    ,Key_RightCurl   ,___   ,___
              ,___   ,Key_LeftParen   ,Key_RightParen  ,___   ,___
